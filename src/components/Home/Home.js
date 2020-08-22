76bb152a22cf88c994697abe8b406e4d6cfd0b92
mamadou sylla <m27sylla@gmail.com>
2020-08-22 16:05:40 +0200

    creation state page home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../../config';
 import HeroImage from '../HeroImage/HeroImage';
 import SearchBar from '../SearchBar/SearchBar';
 import FourColGrid from '../FourColGrid/FourColGrid';
@@ -7,18 +8,31 @@ import MovieThumb from '../MovieThumb/MovieThumb';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 import Spinner from '../Spinner/Spinner';
 import './Home.css';
+import { render } from '@testing-library/react';
 
-function Home() {
-	return (
-		<div className="rmdb-home">
-			<HeroImage />
-			<SearchBar />
-			<FourColGrid />
-			<Spinner/>
-			<LoadMoreBtn />
-			
-		</div>
-	)
+function Home()
+{
+	state = {
+		movies: [],
+		heroImage: null,
+		loading: false,
+		currentPage: 0,
+		totalPages: 0,
+		searchTerm: ''
+
+	}
+	render(){
+		return (
+			<div className="rmdb-home">
+				<HeroImage />
+				<SearchBar />
+				<FourColGrid />
+				<Spinner />
+				<LoadMoreBtn />
+
+			</div>
+		)
+	}
 }
 
 export default Home;
