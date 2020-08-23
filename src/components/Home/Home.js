5ec833fd369129c3f259e25e1b8ca89eff6b206d
mamadou sylla <m27sylla@gmail.com>
2020-08-23 15:40:25 +0200

    partie fetch de la page d'accueil

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Component } from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../../config';
 import HeroImage from '../HeroImage/HeroImage';
 import SearchBar from '../SearchBar/SearchBar';
@@ -10,29 +10,71 @@ import Spinner from '../Spinner/Spinner';
 import './Home.css';
 import { render } from '@testing-library/react';
 
-function Home()
+class Home extends Component
 {
-	state = {
-		movies: [],
-		heroImage: null,
-		loading: false,
-		currentPage: 0,
-		totalPages: 0,
-		searchTerm: ''
+  state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  }
 
-	}
-	render(){
-		return (
-			<div className="rmdb-home">
-				<HeroImage />
-				<SearchBar />
-				<FourColGrid />
-				<Spinner />
-				<LoadMoreBtn />
+  componentDidMount()
+  {
+    this.setState({ loading: true });
+    //affichage des films populaires sur la page d'accueil
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
 
-			</div>
-		)
-	}
+  //variable pour afficher autre films en cliquant sur le bouton load more
+  loadMoreItems = () =>
+  {
+    let endpoint = '';
+    this.setState = ({ loading: true });
+
+    if (this.state.searchTerm === '')
+    {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else
+    {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = (endpoint) =>
+  {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result =>
+      {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages,
+
+        })
+      })
+  }
+
+
+  render()
+  {
+    return (
+      <div className="rmdb-home">
+        <HeroImage />
+        <SearchBar />
+        <FourColGrid />
+        <Spinner />
+        <LoadMoreBtn />
+      </div>
+    )
+  }
 }
 
 export default Home;
