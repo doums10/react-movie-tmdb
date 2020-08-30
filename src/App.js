616c75f59f4584403301d995727d4494820dacee
mamadou sylla <m27sylla@gmail.com>
2020-08-30 21:11:08 +0200

    movie page and link to done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import Movie from './components/Movie/Movie';
 
-function App(){
-	return(
-		<div>
-			<Header />
-			<Home />
-		</div>
+function App()
+{
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/:movieId" component={Movie} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
 	)
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header()
 {
 	return (
 		<div className="rmdb-header">
 			<div className="rmdb-header-content">
-					<img src={require('/Users/doums/code/doums10/react-movie-tmdb/src/images/reactMovie_logo.png')} alt="rmdb.logo" className="rmdb-logo"/>
-					<img src={require('/Users/doums/code/doums10/react-movie-tmdb/src/images/tmdb_logo.png')} alt="tmdb-logo" className="rmdb-tmdb-logo"/>
+			<Link to="/" >
+					<img src="../../images/reactMovie_logo.png" alt="rmdb.logo" className="rmdb-logo"/>
+      </Link>    
+					<img src="../../images/tmdb_logo.png" alt="tmdb-logo" className="rmdb-tmdb-logo"/>
 			</div>
 		</div>
 	)
 }
 
-export default Header
+export default Header;
