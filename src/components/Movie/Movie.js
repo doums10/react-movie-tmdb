780ad98d5ef304259a49747138b2c649c0ea55dd
mamadou sylla <m27sylla@gmail.com>
2020-09-03 00:19:01 +0200

    creation components pour page du film cliqué

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { API_KEY, API_URL } from '../../config';
+import Navigation from '../Navigation/Navigation';
 
-
-class Movie extends Component {
-	render() {
-		return (
-			<div>
-				Movie
-			</div>
-		)
+	class Movie extends Component
+	{
+		render()
+		{
+			return (
+				<div>
+					Movie
+				</div>
+			)
+		}
 	}
-}
 
 export default Movie;
