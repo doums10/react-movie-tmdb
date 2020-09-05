c68202b6524da30ecaef56a7dd64a0a8b4aaddc2
mamadou sylla <m27sylla@gmail.com>
2020-09-05 20:03:16 +0200

    fetch movies creation en cours

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
 import { API_KEY, API_URL } from '../../config';
 import Navigation from '../Navigation/Navigation';
-import MovieInfo from '../MovieInfo/Movieinfo';
+import MovieInfo from '../MovieInfo/MovieInfo';
 import MovieInfoBar from '../MovieInfoBar/MovieInfoBar';
 import FourColGrid from '../FourColGrid/FourColGrid';
-import Actor from '../Actor';
+import Actor from '../Actor/Actor';
 import Spinner from '../Spinner/Spinner';
 import './Movie';
 
 class Movie extends Component
 {
-  render()
+  state = {
+    movie: null,
+    actors: null,
+    directors: [],
+    loading: false
+  }
+
+  componentDidMount()
   {
-    return (
-      <div className="rmdb-movie">
-        <Navigation />
-        <MovieInfo />
-        <MovieInfoBar />
-        <FourColGrid />
-        <Spinner />
-			</div>
-		)
-	}
+    this.setState({ loading: true })
+    //First fetch the movie 
+    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = (endpoint) =>
+  {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result =>
+      {
+
+        if (result.status_code)
+        {
+          this.setState({ loading: false });
+        } else
+        {
+          this.setState({ movie: result }), () =>
+          {
+            // ...then fetch actors in the setState callback function
+            const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`
+            fetch(endpoint)
+              .then(result => result.json())
+              .then(result =>
+              {
+                const directors = result.crew.filter((member) => member.job === "Director");
+
+                this.setState({
+                  actors: result.cast
+                })
+              })
+          })
+  }
+})
+  }
+
+render()
+{
+  return (
+    <div className="rmdb-movie">
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+
+      <Spinner />
+    </div>
+  )
+}
 }
 
 export default Movie;
