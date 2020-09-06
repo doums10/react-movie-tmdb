d72746179aad2514055cc59288b07622c148bb19
mamadou sylla <m27sylla@gmail.com>
2020-09-07 01:44:03 +0200

    actor component done

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { IMAGE_BASE_URL } from '../../config';
-import './Actor';
+import React from "react";
+import { IMAGE_BASE_URL } from "../../config";
+import "./Actor";
 
 function Actor(props) {
-	return (
-		<div>
-			Actor
-		</div>
-	)
+  const POSTER_SIZE = "w154";
+
+  return (
+    <div className="rmdb-actor">
+      <img
+        src={
+          props.actor.profile_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}`
+            : "../../images/no-image.jpg"
+        }
+        alt="actorthumb"
+      />
+      <span className="rmdb-actor-name">{props.actor.name}</span>
+      <span className="rmdb-actor-character">{props.actor.character}</span>
+    </div>
+  );
 }
 
-export default Actor
+export default Actor;
