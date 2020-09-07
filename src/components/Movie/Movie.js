c5c2dba112bf910d6337dc78da5bb40a6d73de03
mamadou sylla <m27sylla@gmail.com>
2020-09-07 02:43:55 +0200

    css à faire sur page movie

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,76 +1,91 @@
-import React, { Component } from 'react';
-import { API_KEY, API_URL } from '../../config';
-import Navigation from '../Navigation/Navigation';
-import MovieInfo from '../MovieInfo/MovieInfo';
-import MovieInfoBar from '../MovieInfoBar/MovieInfoBar';
-import FourColGrid from '../FourColGrid/FourColGrid';
-import Actor from '../Actor/Actor';
-import Spinner from '../Spinner/Spinner';
-import './Movie';
+import React, { Component } from "react";
+import { API_KEY, API_URL } from "../../config";
+import Navigation from "../Navigation/Navigation";
+import MovieInfo from "../MovieInfo/MovieInfo";
+import MovieInfoBar from "../MovieInfoBar/MovieInfoBar";
+import FourColGrid from "../FourColGrid/FourColGrid";
+import Actor from "../Actor/Actor";
+import Spinner from "../Spinner/Spinner";
+import "./Movie";
 
-class Movie extends Component
-{
+class Movie extends Component {
   state = {
     movie: null,
     actors: null,
     directors: [],
     loading: false
-  }
+  };
 
-  componentDidMount()
-  {
-    this.setState({ loading: true })
-    //First fetch the movie 
+  componentDidMount() {
+    this.setState({ loading: true });
+    //First fetch the movie
     const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
     this.fetchItems(endpoint);
   }
 
-  fetchItems = (endpoint) =>
-  {
+  fetchItems = (endpoint) => {
     fetch(endpoint)
-      .then(result => result.json())
-      .then(result =>
-      {
+      .then((result) => result.json())
+      .then((result) => {
         console.log(result);
-        if (result.status_code)
-        {
+        if (result.status_code) {
           this.setState({ loading: false });
-        } else
-        {
-          this.setState({ movie: result }, () =>
-          {
+        } else {
+          this.setState({ movie: result }, () => {
             // ...then fetch actors in the setState callback function
-            const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`
+            const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
             fetch(endpoint)
-              .then(result => result.json())
-              .then(result =>
-              {
-                const directors = result.crew.filter((member) => member.job === "Director");
+              .then((result) => result.json())
+              .then((result) => {
+                const directors = result.crew.filter(
+                  (member) => member.job === "Director"
+                );
 
                 this.setState({
                   actors: result.cast,
                   directors,
                   loading: false
-                })
-              })
-          })
-  }
-})
-   //.catch (error => console.error('Error:', error))
+                });
+              });
+          });
+        }
+      });
+    //.catch (error => console.error('Error:', error))
   };
 
-render()
-{
-  return (
-    <div className="rmdb-movie">
-      <Navigation />
-      <MovieInfo />
-      <MovieInfoBar />
-
-      <Spinner />
-    </div>
-  )
-}
+  render() {
+    return (
+      <div className="rmdb-movie">
+        {this.state.movie ? (
+          <div>
+            <Navigation movie={this.props.location.movieName} />
+            <MovieInfo
+              movie={this.state.movie}
+              directors={this.state.directors}
+            />
+            <MovieInfoBar
+              time={this.state.movie.runtime}
+              budget={this.state.movie.budget}
+              revenur={this.state.movie.revenue}
+            />
+          </div>
+        ) : null}
+        {this.state.actors ? (
+          <div className="rmdb-movie-grid">
+            <FourColGrid header={"Actors"}>
+              {this.state.actors.map((element, i) => {
+                return <Actor key={i} actor={element} />;
+              })}
+            </FourColGrid>
+          </div>
+        ) : null}
+        {!this.state.actors && !this.state.loading ? (
+          <h1>No Movie Found!</h1>
+        ) : null}
+        {this.state.loading ? <Spinner /> : null}}
+      </div>
+    );
+  }
 }
 
 export default Movie;
--- src/components/MovieInfoBar/MovieInfoBar.js
+++ src/components/MovieInfoBar/MovieInfoBar.js
@@ -7,19 +7,19 @@ const MovieInfoBar = (props) => {
   return (
     <div className="rmdb-movieinfobar">
       <div className="rmdb-movieinfobar-content">
-        <div className="rmdb-movieinfobar-col">
+        <div className="rmdb-movieinfobar-content-col">
           <FontAwesome className="fa-time" name="clock-o" size="2x" />
           <span className="rmdb-movieinfobar-info">
             Running Time: {calcTime(props.time)}
           </span>
         </div>
-        <div className="rmdb-movieinfobar-col">
+        <div className="rmdb-movieinfobar-content-col">
           <FontAwesome className="fa-budget" name="money" size="2x " />
           <span className="rmdb-movieinfobar-info">
             Budget: {convertMoney(props.budget)}
           </span>
         </div>
-        <div className="rmdb-movie-infobar-col">
+        <div className="rmdb-movieinfobar-content-col">
           <FontAwesome className="fa-revenue" name="ticket" size="2x" />
           <span className="rmdb-movieinfobar-info">
             Revenue: {convertMoney(props.revenue)}
