96183b2f608176be2fb0c7c353bcad9a5f016177
mamadou sylla <m27sylla@gmail.com>
2020-09-11 13:55:38 +0200

    css cards à continuer, bug revenue moviefinfobar

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import FontAwesome from 'react-fontawesome';
 import MovieThumb from '../MovieThumb/MovieThumb';
-import './Movieinfo.css';
+import './MovieInfo.css';
 
 function MovieInfo(props)
 {
