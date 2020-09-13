4b81a3f174b01cfd3a7a4316fb3fab2e6b5ca80c
mamadou sylla <m27sylla@gmail.com>
2020-09-13 18:00:10 +0200

    typechecking with props testing

--- src/components/FourColGrid/FourColGrid.js
+++ src/components/FourColGrid/FourColGrid.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import './FourColGrid.css';
+import React from "react";
+import "./FourColGrid.css";
+import PropTypes from "prop-types";
 
-const FourColGrid = (props) =>
-{
-
-	const renderElements = () =>
-	{
-		const gridElements = props.children.map((element, i) =>
-		{
-			return (
-				<div key={i} className="rmdb-grid-element">
-					{element}
-					{/*<span className="tooltiptext">{props.text}kkk</span>*/}
-				</div>
-
-			)
-		})
-		return gridElements;
-	}
-	return (
-		<div className="rmdb-grid">
-			{props.header && !props.loading ? <h1>{props.header}</h1> : null}
-			<div className="rmdb-grid-content">
-				{renderElements()}
-			</div>
-		</div>
-	)
-}
+const FourColGrid = (props) => {
+  const renderElements = () => {
+    const gridElements = props.children.map((element, i) => {
+      return (
+        <div key={i} className="rmdb-grid-element">
+          {element}
+          {/*<span className="tooltiptext">{props.text}kkk</span>*/}
+        </div>
+      );
+    });
+    return gridElements;
+  };
+  return (
+    <div className="rmdb-grid">
+      {props.header && !props.loading ? <h1>{props.header}</h1> : null}
+      <div className="rmdb-grid-content">{renderElements()}</div>
+    </div>
+  );
+};
+//test proptypes pour éviter les erreurs de types dans le choix des props
+FourColGrid.propTypes = {
+  header: PropTypes.string,
+  loading: PropTypes.bool
+};
 
 export default FourColGrid;
